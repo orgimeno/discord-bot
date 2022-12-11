42e8b86cf3aa251f801c1c700ef9659cde19cc59
t727662 <OSCAR.RODRIGUEZGIMENO.ext@telefonica.com>
2022-12-11 06:32:07 +0100

    create command interface

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const { readdirSync } = require('fs');
+const { join } = require('path');
+
+const { Client, GatewayIntentBits, Collection, Message } = require('discord.js');
 const client = new Client({ intents: [
   GatewayIntentBits.Guilds, 
   GatewayIntentBits.GuildMessages, 
   GatewayIntentBits.MessageContent
 ]});
+const commands = new Collection() as Collection<string, Command>;
+
+const commanddFiles = readdirSync(join(process.cwd(), 'dist', 'commands'))
+  .filter(file => file.endsWith('.js'));
+
+console.log(commanddFiles);
+
+interface Command {
+  default: {
+    name: string;
+    description: string;
+    execute: (message: Message, args: string[]) => void | Promise<void>;
+  };
+}
 
 client.on('ready', () => {
   console.log("Bot is ready");
@@ -13,6 +30,6 @@ client.on('ready', () => {
 client.on('messageCreate', (message: any) => {
   if(message.content === 'ping' ){
     message.channel.send('pong');
-  }});
+}});
 
 client.login(process.env.TOKEN);
